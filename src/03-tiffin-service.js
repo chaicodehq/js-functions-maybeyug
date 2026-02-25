0e3dbdd0e16bba6d0521c9360503b95bea582fda
Yug Saini <41270627+Yug-Singh@users.noreply.github.com>
2026-02-26 03:51:33 +0530

    Complete 03-tiffin

--- src/03-tiffin-service.js
+++ src/03-tiffin-service.js
@@ -40,13 +40,101 @@
  *   // => { totalCustomers: 3, totalRevenue: 7200, mealBreakdown: { veg: 2, nonveg: 1 } }
  */
 export function createTiffinPlan({ name, mealType = "veg", days = 30 } = {}) {
-  // Your code here
+  // Your code here\
+  if (name === undefined || name === "") {
+    return null;
+  }
+
+  const vegMealDailyRate = 80;
+  const nonVeMealDailyRate = 120;
+  const jainMealDailyRate = 90;
+  let calTotalCost;
+
+  function calculateCost(dailyRate) {
+    calTotalCost = dailyRate * days;
+    return {
+      name: name,
+      mealType: mealType,
+      days: days,
+      dailyRate: dailyRate,
+      totalCost: calTotalCost,
+    };
+  }
+
+  switch (mealType) {
+    case "veg":
+      return calculateCost(vegMealDailyRate);
+
+    case "nonveg":
+      return calculateCost(nonVeMealDailyRate);
+
+    case "jain":
+      return calculateCost(jainMealDailyRate);
+
+    default:
+      return null;
+  }
 }
 
 export function combinePlans(...plans) {
   // Your code here
+
+  if (Array.isArray(plans) && plans.length === 0) return null;
+
+  let calTotalCustomer = 0,
+    calTotalRevenue = 0,
+    calVegMeal = 0,
+    calNonvegMeal = 0,
+    calJainMeal = 0;
+
+  plans.forEach((item) => {
+    calTotalCustomer++;
+    calTotalRevenue += item.totalCost;
+    if (item.mealType === "veg") calVegMeal++;
+    else if (item.mealType === "nonveg") calNonvegMeal++;
+    else calJainMeal++;
+  });
+
+  return {
+    totalCustomers: calTotalCustomer,
+    totalRevenue: calTotalRevenue,
+    mealBreakdown: {
+      veg: calVegMeal,
+      nonveg: calNonvegMeal,
+      jain: calJainMeal,
+    },
+  };
 }
 
 export function applyAddons(plan, ...addons) {
   // Your code here
+  if (
+    plan === null ||
+    Object.keys(plan).length === 0 ||
+    plan.name === null ||
+    plan.mealType === null ||
+    plan.days === null ||
+    plan.dailyRate === null
+  ) {
+    return null;
+  }
+  let addAddonsPrice = 0;
+  let addAddonsName = [];
+  let newTotalCost;
+  let newDailyRate;
+  addons.forEach((item) => {
+    addAddonsPrice += item.price;
+    addAddonsName.push(item.name);
+  });
+  newDailyRate = plan.dailyRate + addAddonsPrice;
+  newTotalCost = newDailyRate * plan.days;
+
+  return {
+    name: plan.name,
+    mealType: plan.mealType,
+    days: plan.days,
+    dailyRate: newDailyRate,
+    totalCost: newTotalCost,
+    addonNames: addAddonsName,
+  };
 }
